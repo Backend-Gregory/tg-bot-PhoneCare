dcb1500a19c247a62750f313c65d498cb2004b9a
Frontend-Gregory <okishevgriha@gmail.com>
2026-04-29 22:18:31 +0300

    Add file logging: save logs to bot.log with UTF-8 encoding

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ from handlers import router
 from middlewares import RateLimitMiddleware
 from aiogram.types import BotCommandScopeChat, BotCommand
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[
+        logging.FileHandler("bot.log", encoding='utf-8'),
+        logging.StreamHandler()
+    ]
+    )
 
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
